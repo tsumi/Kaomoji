11594c549433aee96d0ada2b3da3e4269ced5cf2
Andrea Boero <mail@tsumi.it>
2014-12-28 18:04:07 +0100

    Fixed a bug in buildjson script

--- buildjson.php
+++ buildjson.php
@@ -4,79 +4,84 @@ $kaomoji=array();
 // Category: POSITIVE
 $category=array();
 $category["id"]="positive";
+$category["sections"]=array();
 
 $section=array();
 $section["id"]="joy";
 $section["kaomoji"]=array("(*^ω^)", "(-‿‿-)", "(o^▽^o)", "<(￣︶￣)>", "(￣ω￣)", "(o･ω･o)", "(＠＾－＾)", "ヽ(*・ω・)ﾉ", "(^人^)", "(o´▽`o)", "｡ﾟ( ﾟ^∀^ﾟ)ﾟ｡", "(´ω｀)", "(☆▽☆)", "(≧◡≦)", "(´• ω •`)", "(＾▽＾)", "(⌒ω⌒)", "╰(▔∀▔)╯", "(─‿‿─)", "(*^‿^*)", "ヽ(o^―^o)ﾉ", "(✯◡✯)", "(◕‿◕)", "(*≧ω≦*)", "(((o(*ﾟ▽ﾟ*)o)))", "(⌒‿⌒)", "＼(≧▽≦)／", "⌒(o＾▽＾o)ノ", "☆ ～('▽^人)", "(*ﾟ▽ﾟ*)", "(✧ω✧)", "(´｡• ᵕ •｡`)", "(￣▽￣)", "╰(*´︶`*)╯", "o(≧▽≦)o", "(☆ω☆)", "(っ˘ω˘ς )", "｡◕‿◕｡", "( ͡° ͜ʖ ͡°)", "( ⋂‿⋂’)", "(-^〇^-)", "( ﾟ▽ﾟ)/", "（〜^∇^)〜", "(〜￣▽￣)〜", "(・◇・)", "(*‿*✿)", "(*⌒▽⌒*)θ～♪", "(∩▂∩)", "(✌ﾟ∀ﾟ)☞", "＼(^ω^＼)", "(／^ω^)／", "❣◕ ‿ ◕❣", "◉‿◉");
-$category["sections"]=array($section);
-$kaomoji['category'][]=$category;
+$category["sections"][]=$section;
 $section=array();
 $section["id"]="love";
 $section["kaomoji"]=array("(ﾉ´з｀)ノ", "(♡μ_μ)", "(*^^*)♡", "☆⌒ヽ(*'､^*)chu", "(￣ε￣＠)", "ヽ(♡‿♡)ノ", "( ´∀｀)ノ～ ♡", "(─‿‿─)♡", "(´ω｀♡)", "( ◡‿◡ ♡)", "(◕‿◕)♡", "(ღ˘⌣˘ღ)", "( ื▿ ืʃƪ)", "(♡ﾟ▽ﾟ♡)", "♡(。-ω-)", "(´ε｀ )♡", "(∩˃o˂∩)♡", "(∿°○°)∿ ︵ ǝʌol", "(灬ºωº灬)♡", "♥ω♥ ) ~♪", "(◞ꈍ∇ꈍ)◞⋆**✚⃞ྉ ");
-$category["sections"]=array($section);
-$kaomoji['category'][]=$category;
+$category["sections"][]=$section;
 $section=array();
 $section["id"]="ebarrassment";
 $section["kaomoji"]=array("(⌒_⌒;)", "(o^ ^o)", "(*/ω＼)", "(*/_＼)", "(*ﾉωﾉ)", "(o-_-o)", "(ᵔ.ᵔ)", "(*ﾉ∀`*)", "(//▽//)", "(//ω//)", "(ノ*ﾟ▽ﾟ*)", "(*^.^*)", "(〃￣ω￣〃ゞ", "(#｀ε´#ゞ", "(o・・o)/", "⁄(⁄ ⁄•⁄-⁄•⁄ ⁄)⁄", "(∗∕ ∕•̥̥̥̥∕ω∕•̥̥̥̥∕)", "ԅ[ •́ ﹏├┬┴┬┴", "ヾ(〃ﾟーﾟ〃)ノ", "(●///▽///●)");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
+
 $kaomoji['category'][]=$category;
 
 // Category: NEGATIVE
 $category=array();
 $category["id"]="negative";
+$category["sections"]=array();
 
 $section=array();
 $section["id"]="sad";
 $section["kaomoji"]=array("( ≧Д≦)", "( p_q)", "((´д｀))", "(╯︵╰,)", "(✖╭╮✖)", "ヽ(´□｀。)ﾉ 	", ".·´¯`(>▂<)´¯`·.", "｡･ﾟﾟ･(>д<)･ﾟﾟ･｡", "｡ﾟ( ﾟஇ‸இﾟ)ﾟ｡", "（ ＴДＴ）", "(-̩̩̩-̩̩̩-̩̩̩-̩̩̩-̩̩̩___-̩̩̩-̩̩̩-̩̩̩-̩̩̩-̩̩̩) 	", "（；_・）", "(′︿‵｡)", "(━┳━ _ ━┳━)", "(╥_╥)", "(个_个)", "╥﹏╥", "o(╥﹏╥)o", "ಥ_ಥ", "(*꒦ິ⌓꒦ີ)", "o(TﾍTo)", "●︿●");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
 $section=array();
 $section["id"]="angry";
 $section["kaomoji"]=array("( ≧Д≦)", "((╬ಠิ﹏ಠิ))", "(」゜ロ゜)」", "凸(｀0´)凸", "(/ﾟДﾟ)/", "( ꒪Д꒪)ノ", "(҂⌣̀_⌣́)", "ヽ༼ ಠ益ಠ ༽ﾉ", "(╬ﾟ◥益◤ﾟ)", "(ノಠ ∩ಠ)ノ彡( o°o)", "(╯°□°）╯︵ ┻━┻ 	", "(ノಠ益ಠ)ノ", "凸(⊙▂⊙✖ )", "(ノಠ益ಠ)ノ彡┻━┻", "눈_눈", "(⁎˃ᆺ˂)", "(ᗒᗣᗕ)՞", "૮(ꂧ᷆⺫ꂧ᷇)ა", "(,,Ծ‸Ծ,, )", "(╬ Ò ‸ Ó)", "(╬•᷅д•᷄╬)", "(ﾉꐦ ◎曲◎)ﾉ=͟͟͞͞ ⌨", "( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃", "(ʘ言ʘ╬)", "( ︶︿︶)_╭∩╮");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
 
 $kaomoji['category'][]=$category;
 
 // Category: NEUTRAL
 $category=array();
 $category["id"]="neutral";
+$category["sections"]=array();
 
 $section=array();
 $section["id"]="cry";
 $section["kaomoji"]=array("(*^ω^)", "(*^ω^)");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
 
 $kaomoji['category'][]=$category;
 
 // Category: ANIMALS
 $category=array();
 $category["id"]="animals";
+$category["sections"]=array();
 
 $section=array();
 $section["id"]="cry";
 $section["kaomoji"]=array("(*^ω^)", "(*^ω^)");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
 
 $kaomoji['category'][]=$category;
 
 // Category: ACTIONS
 $category=array();
 $category["id"]="actions";
+$category["sections"]=array();
 
 $section=array();
 $section["id"]="cry";
 $section["kaomoji"]=array("(*^ω^)", "(*^ω^)");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
 
 $kaomoji['category'][]=$category;
 
 // Category: OTHERS
 $category=array();
 $category["id"]="others";
+$category["sections"]=array();
 
 $section=array();
 $section["id"]="cry";
 $section["kaomoji"]=array("(*^ω^)", "(*^ω^)");
-$category["sections"]=array($section);
+$category["sections"][]=$section;
 
 $kaomoji['category'][]=$category;
 
